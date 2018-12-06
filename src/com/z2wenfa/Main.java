e0176b6a8d387b5a1256499959f2d88faca62de4
z2wenfa <z2wenfa@163.com>
2018-12-06 11:09:43 +0800

    添加归并排序

--- src/com/z2wenfa/Main.java
+++ src/com/z2wenfa/Main.java
@@ -6,17 +6,17 @@ import java.util.Random;
 
 public class Main {
 
-    static Class<IArithmetic>[] sortArrs = new Class[]{BubbleSort.class, QuickSort.class, SelectionSort.class, InsertionSort.class, ShellSort.class};
+    static Class<IArithmetic>[] sortArrs = new Class[]{BubbleSort.class, QuickSort.class, SelectionSort.class,
+            InsertionSort.class, ShellSort.class, MergeSort.class};
 
     public static void main(String[] args) {
         Random random = new Random();
         int[] array = new int[20];
-        for (int i = 0; i < 20; i++) {
-            array[i] = random.nextInt(10000);
-        }
-
         for (Class<IArithmetic> sort : sortArrs) {
             try {
+                for (int i = 0; i < 20; i++) {
+                    array[i] = random.nextInt(10000);
+                }
                 IArithmetic arithmetic = sort.newInstance();
                 arithmetic.runAndPrint(array);
             } catch (Exception e) {
---
+++ src/com/z2wenfa/sort/MergeSort.java
@@ -0,0 +1,55 @@
+package com.z2wenfa.sort;
+
+import com.z2wenfa.IArithmetic;
+
+public class MergeSort extends IArithmetic {
+    @Override
+    public void run() {
+        megerSort(arrs);
+    }
+
+    @Override
+    public String getArithmeticName() {
+        return "归并排序";
+    }
+
+    private void megerSort(int[] arrs) {
+        sort(arrs, 0, arrs.length - 1);
+    }
+
+    private void sort(int[] arrs, int low, int high) {
+        int mid = (low + high) / 2;
+        if (low < high) {
+            sort(arrs, low, mid);
+            sort(arrs, mid + 1, high);
+            merge(arrs, low, mid, high);
+        }
+    }
+
+    private void merge(int[] arrs, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= high) {
+            if (arrs[i] < arrs[j]) {
+                temp[k++] = arrs[i++];
+            } else {
+                temp[k++] = arrs[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arrs[i++];
+        }
+
+        while (j <= high) {
+            temp[k++] = arrs[j++];
+        }
+
+        k = 0;
+        while (low <= high) {
+            arrs[low++] = temp[k++];
+        }
+    }
+}
