e7dbb59ba777c1ee68efb52ff9e969ad9bbb455c
z2wenfa <z2wenfa@163.com>
2021-04-12 22:19:07 +0800

    优化算法

--- src/com/z2wenfa/sort/InsertionSort.java
+++ src/com/z2wenfa/sort/InsertionSort.java
@@ -1,8 +1,14 @@
 package com.z2wenfa.sort;
 
 import com.z2wenfa.base.ISortArithmetic;
+import com.z2wenfa.util.ArrUtil;
 
 public class InsertionSort extends ISortArithmetic {
+
+    public static void main(String[] args) {
+        new InsertionSort().execute();
+    }
+
     @Override
     public void sort() {
         insertionSort(arrs);
@@ -17,12 +23,21 @@ public class InsertionSort extends ISortArithmetic {
         for (int i = 1; i < arrs.length; i++) {
             int j = i;
             while (j > 0 && arrs[j] < arrs[j - 1]) {
-                int temp = arrs[j];
-                arrs[j] = arrs[j - 1];
-                arrs[j - 1] = temp;
+                ArrUtil.exchangeArrValue(arrs, j, j - 1);
                 j--;
             }
         }
     }
+
+    private void insertionSortTest(int arrs[]) {
+        for (int i = 1; i < arrs.length; i++) {
+            int j = i;
+            while (j > 0 && arrs[j] < arrs[j - 1]) {
+                ArrUtil.exchangeArrValue(arrs, j, j - 1);
+                j--;
+            }
+        }
+    }
+
 
 }
--- src/com/z2wenfa/sort/ShellSort.java
+++ src/com/z2wenfa/sort/ShellSort.java
@@ -1,12 +1,17 @@
 package com.z2wenfa.sort;
 
 import com.z2wenfa.base.ISortArithmetic;
+import com.z2wenfa.util.ArrUtil;
 
 public class ShellSort extends ISortArithmetic {
 
+    public static void main(String[] args) {
+        new ShellSort().execute();
+    }
+
     @Override
     public void sort() {
-        shellSort(arrs);
+        shellSortTest(arrs);
     }
 
     @Override
@@ -20,15 +25,23 @@ public class ShellSort extends ISortArithmetic {
         while (increment >= 1) {
             for (int i = 0; i < arrs.length; i++) {
                 for (int j = i + increment; j < arrs.length; j = j + increment) {
-                    if (arrs[j] < arrs[j - increment]) {
-                        int temp = arrs[j];
-                        arrs[j] = arrs[j - increment];
-                        arrs[j - increment] = temp;
-                    }
+                    if (arrs[j] < arrs[j - increment]) ArrUtil.exchangeArrValue(arrs, j, j - increment);
                 }
             }
             increment = increment / 2;
         }
+    }
 
+    private void shellSortTest(int[] arrs) {
+        if (arrs == null || arrs.length <= 1) return;
+        int increment = arrs.length / 2;
+        while (increment >= 1) {
+            for (int i = 0; i < arrs.length; i++) {
+                for (int j = i + increment; j < arrs.length; j = j + increment) {
+                    if (arrs[j] < arrs[j - increment]) ArrUtil.exchangeArrValue(arrs, j, j - increment);
+                }
+            }
+            increment = increment/ 2;
+        }
     }
 }
